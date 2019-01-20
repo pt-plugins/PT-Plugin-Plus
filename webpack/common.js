c66a5946c7e9b020d44642df164124068a2e5bf6
Rhilip <rhilipruan@gmail.com>
2019-01-20 12:02:44 +0800

    fix: 移除错误的排除项

--- webpack/common.js
+++ webpack/common.js
@@ -39,7 +39,7 @@ module.exports = {
     new CopyWebpackPlugin([{
       from: resolve('/resource/'),
       to: path.join(resolve('/dist/'), 'resource'),
-      ignore: ["*.js","README.md"]
+      ignore: ["README.md"]
     }])
   ]
 };
