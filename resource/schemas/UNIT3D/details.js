d0750a6ebf4f46eadf10827c641d2a67c7fb7663
栽培者 <ronggang.zhou@gmail.com>
2019-02-24 10:19:13 +0800

    fix: 修复 UNIT3D 架构种子详情页面插件图标加载重复的Bug

--- resource/schemas/UNIT3D/details.js
+++ resource/schemas/UNIT3D/details.js
@@ -3,6 +3,9 @@
   class App extends window.NexusPHPCommon {
     init() {
       this.initButtons()
+      this.initFreeSpaceButton()
+      // 设置当前页面
+      PTSevrice.pageApp = this;
     }
     /**
      * 初始化按钮列表
