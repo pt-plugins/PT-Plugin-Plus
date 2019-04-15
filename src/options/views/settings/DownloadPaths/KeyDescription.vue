90683c29335bf80883c45e1020f0707a75cc54d8
栽培者 <ronggang.zhou@gmail.com>
2019-04-15 23:31:21 +0800

    refactor: 消除 keyDescription 因没有默认导出vscode的错误提示

--- src/options/views/settings/DownloadPaths/KeyDescription.vue
+++ src/options/views/settings/DownloadPaths/KeyDescription.vue
@@ -4,25 +4,33 @@
     <table>
       <tr>
         <td>$site.name$</td>
-        <td>将被替换为当前站点名称；
+        <td>
+          将被替换为当前站点名称；
           <br>例：/volume1/$site.name$/music -> /volume1/OpenCD/music
         </td>
       </tr>
       <tr>
         <td>$site.host$</td>
-        <td>将被替换为当前站点域名；
+        <td>
+          将被替换为当前站点域名；
           <br>例：/volume1/$site.host$/music -> /volume1/open.cd/music
         </td>
       </tr>
       <tr>
         <td>&lt;...&gt;</td>
-        <td>将会弹出输入框，由用户输入路径中 &lt;...&gt; 部分内容；
+        <td>
+          将会弹出输入框，由用户输入路径中 &lt;...&gt; 部分内容；
           <br>例：/volume1/&lt;...&gt; ，输入：test，结果 /volume1/test
         </td>
       </tr>
     </table>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
 <style lang="scss" scoped>
 .keyDescription {
   padding-top: 10px;
