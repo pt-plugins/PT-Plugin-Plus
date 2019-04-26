a3d10b802938f4a052060ddaa1b257699b93aec6
栽培者 <ronggang.zhou@gmail.com>
2019-04-26 22:38:43 +0800

    feat: 豆瓣选电影页增加PT搜索支持
    当在电影海报上悬停显示电影信息时，增加搜索按钮，点击可以直接进行搜索

--- resource/publicSites/douban.com/subject.js
+++ resource/publicSites/douban.com/subject.js
@@ -1,33 +1,26 @@
 (function ($, window) {
-  class App {
-    init() {
-      this.initButtons();
-      // 设置当前页面
-      PTService.pageApp = this;
-    }
-
+  class App extends window.DoubanCommon {
     /**
      * 初始化按钮列表
      */
     initButtons() {
-
-      let IMDbId = this.getIMDbId();
-      if (IMDbId) {
+      let key = this.getIMDbId() || this.getTitle();
+      if (key) {
         // 搜索
         PTService.addButton({
           title: "搜索当前电影",
           icon: "search",
           label: "搜索",
           click: (success, error) => {
-            PTService.call(PTService.action.openOptions, `search-torrent/${IMDbId}`);
+            this.search(key);
             success();
           }
         });
 
         let recommendButton = $("span.rec a[share-id]");
         if (recommendButton.length > 0) {
-          $("<a href='javascript:void(0);' class='lnk-sharing' style='margin-right: 5px;'/>").html("用 PT 助手搜索").on("click", () => {
-            PTService.call(PTService.action.openOptions, `search-torrent/${IMDbId}`);
+          $("<a href='javascript:void(0);' class='lnk-sharing' style='margin-right: 5px;'/>").html("用 PT 助手搜索").on("click", (event) => {
+            this.search(key, $(event.target));
           }).insertBefore(recommendButton);
         }
       }
@@ -43,6 +36,10 @@
       }
 
       return "";
+    }
+
+    getTitle() {
+      return document.title.replace(" (豆瓣)", "");
     }
   };
   (new App()).init();
