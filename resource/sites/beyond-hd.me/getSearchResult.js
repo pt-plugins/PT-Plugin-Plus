87ceeea9e9c5e95a822d7f25dc741ee32b6021e4
栽培者 <ronggang.zhou@gmail.com>
2019-06-26 18:24:15 +0800

    docs: 增加 PTP, Beyond-hd By @lengmianxia

---
+++ resource/sites/beyond-hd.me/getSearchResult.js
@@ -0,0 +1,289 @@
+(function(options) {
+  class Parser {
+    constructor() {
+      this.haveData = false;
+      if (/\/login/.test(options.responseText)) {
+        options.status = ESearchResultParseStatus.needLogin;
+        return;
+      }
+
+      options.isLogged = true;
+
+      this.haveData = true;
+    }
+
+    /**
+     * 获取搜索结果
+     */
+    getResult() {
+      if (!this.haveData) {
+        return [];
+      }
+      let site = options.site;
+      let selector =
+        options.resultSelector || "div.table-responsive > table:last";
+      let table = options.page.find(selector);
+      // 获取种子列表行
+      let rows = table.find("> tbody > tr");
+      console.log("rows.length", rows.length);
+      if (rows.length == 0) {
+        options.status = ESearchResultParseStatus.torrentTableIsEmpty;
+        return [];
+      }
+      let results = [];
+      // 获取表头
+      let header = table.find("> thead > tr > th");
+      let beginRowIndex = 0;
+      if (header.length == 0) {
+        beginRowIndex = 1;
+        header = rows.eq(0).find("th,td");
+      }
+      console.log("header.length", header.length);
+
+      // 用于定位每个字段所列的位置
+      let fieldIndex = {
+        // 发布时间
+        time: -1,
+        // 大小
+        size: -1,
+        // 上传数量
+        seeders: -1,
+        // 下载数量
+        leechers: -1,
+        // 完成数量
+        completed: -1,
+        // 评论数量
+        comments: -1,
+        // 发布人
+        author: header.length - 1,
+        // 分类
+        category: 1
+      };
+
+      if (site.url.lastIndexOf("/") != site.url.length - 1) {
+        site.url += "/";
+      }
+
+      // 获取字段所在的列
+      for (let index = 0; index < header.length; index++) {
+        let cell = header.eq(index);
+        let text = cell.text();
+
+        // 评论数
+        if (cell.find("a[href*='comments']").length) {
+          fieldIndex.comments = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 发布时间
+        if (
+          cell.find("a[href*='created_at']").length ||
+          cell.find("i.fa-clock").length
+        ) {
+          fieldIndex.time = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 大小
+        if (
+          cell.find("a[href*='size']").length ||
+          cell.find("i.fa-file").length
+        ) {
+          fieldIndex.size = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 种子数
+        if (
+          cell.find("a[href*='seed']").length ||
+          cell.find("i.fa-arrow-circle-up").length
+        ) {
+          fieldIndex.seeders = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 下载数
+        if (
+          cell.find("a[href*='leech']").length ||
+          cell.find("i.fa-arrow-circle-down").length
+        ) {
+          fieldIndex.leechers = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 完成数
+        if (
+          cell.find("a[href*='complete']").length ||
+          cell.find("i.fa-check-square").length
+        ) {
+          fieldIndex.completed = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+
+        // 分类
+        if (cell.is(".torrents-icon")) {
+          fieldIndex.category = index;
+          fieldIndex.author =
+            index == fieldIndex.author ? -1 : fieldIndex.author;
+          continue;
+        }
+      }
+
+      try {
+        // 遍历数据行
+
+        for (let index = beginRowIndex; index < rows.length; index++) {
+          const row = rows.eq(index);
+          let cells = row.find(">td");
+
+          let title = row.find("a.torrent-name");
+
+          console.log("title", title);
+          console.log("title.length", title.length);
+
+          if (title.length == 0) {
+            continue;
+          }
+          let link = title.attr("href");
+          if (link && link.substr(0, 4) !== "http") {
+            link = `${site.url}${link}`;
+          }
+
+          // 获取下载链接
+          let url = row.find("a[href*='/download/']").attr("href");
+          console.log("index", index);
+          console.log("url.length", url.length);
+          if (url.length == 0) {
+            continue;
+          }
+
+          if (url && url.substr(0, 4) !== "http") {
+            url = `${site.url}${url}`;
+          }
+
+          let data = {
+            title: title.text(),
+            subTitle: this.getSubTitle(title, row),
+            link,
+            url: url,
+            size:
+              cells
+                .eq(fieldIndex.size)
+                .text()
+                .trim() || 0,
+            time:
+              fieldIndex.time == -1
+                ? ""
+                : cells
+                    .eq(fieldIndex.time)
+                    .find("span[title]")
+                    .attr("title") ||
+                  cells.eq(fieldIndex.time).text() ||
+                  "",
+            author:
+              fieldIndex.author == -1
+                ? ""
+                : cells.eq(fieldIndex.author).text() || "",
+            seeders:
+              fieldIndex.seeders == -1
+                ? ""
+                : cells.eq(fieldIndex.seeders).text() || 0,
+            leechers:
+              fieldIndex.leechers == -1
+                ? ""
+                : cells.eq(fieldIndex.leechers).text() || 0,
+            completed:
+              fieldIndex.completed == -1
+                ? ""
+                : cells.eq(fieldIndex.completed).text() || 0,
+            comments:
+              fieldIndex.comments == -1
+                ? ""
+                : cells.eq(fieldIndex.comments).text() || 0,
+            site: site,
+            tags: this.getTags(row, options.torrentTagSelectors),
+            entryName: options.entry.name,
+            category:
+              fieldIndex.category == -1
+                ? null
+                : this.getCategory(cells.eq(fieldIndex.category))
+          };
+          results.push(data);
+        }
+        if (results.length == 0) {
+          options.status = ESearchResultParseStatus.noTorrents;
+        }
+      } catch (error) {
+        console.log(error);
+        options.status = ESearchResultParseStatus.parseError;
+        options.errorMsg = error.stack;
+      }
+
+      return results;
+    }
+
+    /**
+     * 获取标签
+     * @param {*} row
+     * @param {*} selectors
+     * @return array
+     */
+    getTags(row, selectors) {
+      let tags = [];
+      if (selectors && selectors.length > 0) {
+        selectors.forEach(item => {
+          if (item.selector) {
+            let result = row.find(item.selector);
+            if (result.length) {
+              tags.push({
+                name: item.name,
+                color: item.color
+              });
+            }
+          }
+        });
+      }
+      return tags;
+    }
+
+    /**
+     * 获取副标题
+     * @param {*} title
+     * @param {*} row
+     */
+    getSubTitle(title, row) {
+      return "";
+    }
+
+    /**
+     * 获取分类
+     * @param {*} cell 当前列
+     */
+    getCategory(cell) {
+      let result = {
+        name: cell.find("i:first").attr("data-original-title"),
+        link: cell.find("a:first").attr("href")
+      };
+      if (result.name) {
+        result.name = result.name.replace(" torrent", "");
+      }
+      return result;
+    }
+  }
+
+  let parser = new Parser(options);
+  options.results = parser.getResult();
+  console.log(options.results);
+})(options);
---
+++ resource/sites/passthepopcorn.me/getSearchResult.js
@@ -0,0 +1,104 @@
+if (!"".getQueryString) {
+  String.prototype.getQueryString = function(name, split) {
+    if (split == undefined) split = "&";
+    var reg = new RegExp(
+        "(^|" + split + "|\\?)" + name + "=([^" + split + "]*)(" + split + "|$)"
+      ),
+      r;
+    if ((r = this.match(reg))) return decodeURI(r[2]);
+    return null;
+  };
+}
+
+(function(options) {
+  class Parser {
+    constructor() {
+      this.haveData = false;
+      this.categories = {};
+      if (/auth_form/.test(options.responseText)) {
+        options.status = ESearchResultParseStatus.needLogin;
+        return;
+      }
+
+      options.isLogged = true;
+      this.haveData = true;
+    }
+
+    /**
+     * 获取搜索结果
+     */
+
+    getResult() {
+      if (!this.haveData) {
+        return [];
+      }
+      let site = options.site;
+      // 获取种子列表行
+      let Movies = options.page.Movies;
+      if (Movies.length == 0) {
+        options.status = ESearchResultParseStatus.noTorrents;
+        return [];
+      }
+      let results = [];
+      let authkey = options.page.AuthKey;
+      let passkey = options.page.PassKey;
+      console.log("Movies.length", Movies.length);
+      //console.log("Movies", Movies.text());
+      try {
+        // 遍历数据行
+        for (let index = 0; index < Movies.length; index++) {
+          let row = Movies[index];
+          let torrent = row.Torrents[0];
+          let data = {
+            title:
+              row.Title +
+              "[" +
+              row.Year +
+              "]" +
+              "-" +
+              torrent.Codec +
+              "/" +
+              torrent.Container +
+              "/" +
+              torrent.Source +
+              "/" +
+              torrent.Resolution,
+            subTitle: torrent.ReleaseName,
+            link: `${site.url}torrents.php?id=${row.GroupId}&torrentid=${
+              torrent.Id
+            }`,
+            url: `${site.url}torrents.php?action=download&id=${
+              torrent.Id
+            }&authkey=${authkey}&torrent_pass=${passkey}`,
+            size: parseFloat(torrent.Size),
+            time: torrent.UploadTime,
+            author: "",
+            seeders: torrent.Seeders,
+            leechers: torrent.Leechers,
+            completed: torrent.Snatched,
+            comments: 0,
+            site: site,
+            tags: null,
+            entryName: options.entry.name,
+            category: "Movie"
+          };
+          results.push(data);
+        }
+        console.log("results.length", results.length);
+        if (results.length == 0) {
+          options.status = ESearchResultParseStatus.noTorrents;
+        }
+      } catch (error) {
+        console.log(error);
+        options.status = ESearchResultParseStatus.parseError;
+        options.errorMsg = error.stack;
+      }
+
+      return results;
+    }
+  }
+
+  let parser = new Parser(options);
+  options.results = parser.getResult();
+  console.log(options.results);
+})(options);
