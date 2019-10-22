a5ef9f9ba1fae4c85370f6d968dc7c0a1b2ff36d
栽培者 <ronggang.zhou@gmail.com>
2019-10-22 23:33:33 +0800

    chore: 增加 terser-webpack-plugin 插件
    避免 build 后因字符编码问题导致 Chrome 无法加载 content.js 的Bug

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,6 +1,19 @@
-const merge = require('webpack-merge');
-const common = require('./common.js');
+const merge = require("webpack-merge");
+const common = require("./common.js");
+// 避免编译后 Chrome 无法加载 content.js 的问题
+// Chrome 会报以下错误：无法为内容脚本加载“js/content.js”文件。该文件采用的不是 UTF-8 编码。
+// @see https://stackoverflow.com/questions/55601774/chrome-extension-has-a-content-js-error-about-utf-8
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: "production",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: { ascii_only: true }
+        }
+      })
+    ]
+  }
 });
