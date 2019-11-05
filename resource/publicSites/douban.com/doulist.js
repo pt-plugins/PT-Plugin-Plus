bf6d4c233d4e13fae540f7d7f36cb4e6fb5cb0e0
栽培者 <ronggang.zhou@gmail.com>
2019-11-05 22:19:52 +0800

    feat: 增加豆瓣豆列页面搜索按钮适配

---
+++ resource/publicSites/douban.com/doulist.js
@@ -0,0 +1,54 @@
+(function($, window) {
+  class App extends window.DoubanCommon {
+    /**
+     * 初始化按钮列表
+     */
+    initButtons() {
+      let items = $(".doulist-subject");
+      console.log(items.length);
+      if (items.length > 0) {
+        items.each((index, item) => {
+          let $item = $(item);
+          let $title = $item.find(".title a");
+          let link = $title.attr("href");
+          let match = link.match(/subject\/(\d+)/);
+          if (match && match.length >= 2) {
+            let title = $title.text().trim();
+            let key = "";
+
+            switch (true) {
+              // 电影
+              case link.indexOf("movie.douban.com") > 0:
+                key = `douban${match[1]}`;
+                break;
+
+              // 其他
+              default:
+                key = title;
+                break;
+            }
+            this.createButton($item.find(".post"), key, title);
+          }
+        });
+      }
+    }
+
+    createButton(parent, key, title) {
+      if (!key) {
+        return;
+      }
+      let label = "PT 助手搜索";
+      let div = $("<div style='padding: 5px;'/>")
+        .attr("title", `搜索 ${title}`)
+        .appendTo(parent);
+      $("<a href='javascript:void(0);' class='lnk-sharing'/>")
+        .html(label)
+        .on("click", event => {
+          let button = $(event.target);
+          this.search(key, button);
+        })
+        .appendTo(div);
+    }
+  }
+  new App().init();
+})(jQuery, window);
