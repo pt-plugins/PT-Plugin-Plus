5f94c4e9dc32aec1595328529413ba617a7f19d5
栽培者 <ronggang.zhou@gmail.com>
2019-11-21 16:39:17 +0800

    chore: 编译完成时显示完成时间

--- debug/src/buildResource.ts
+++ debug/src/buildResource.ts
@@ -3,3 +3,4 @@ import { BuildPlugin } from "./BuildPlugin";
 let buildPlugin = new BuildPlugin();
 buildPlugin.buildResource();
 buildPlugin.getSupportedSites();
+console.log("编译完成于：%s \n", new Date().toLocaleString());
