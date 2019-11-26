8970869eecbf53b978248e472d8b428ddb6c48f3
栽培者 <ronggang.zhou@gmail.com>
2019-11-26 20:09:09 +0800

    feat: 下载历史启用过滤功能

--- src/options/views/History.vue
+++ src/options/views/History.vue
@@ -3,21 +3,29 @@
     <v-alert :value="true" type="info">{{ $t("history.title") }}</v-alert>
     <v-card>
       <v-card-title>
-        <v-btn color="error" :disabled="selected.length==0">
+        <v-btn color="error" :disabled="selected.length == 0">
           <v-icon class="mr-2">remove</v-icon>
           {{ $t("history.remove") }}
         </v-btn>
 
-        <v-btn color="error" @click="clear" :disabled="items.length==0">
+        <v-btn color="error" @click="clear" :disabled="items.length == 0">
           <v-icon class="mr-2">clear</v-icon>
           {{ $t("history.clear") }}
         </v-btn>
         <v-spacer></v-spacer>
 
-        <v-text-field class="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+        <v-text-field
+          class="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+          v-model="filterKey"
+        ></v-text-field>
       </v-card-title>
 
       <v-data-table
+        :search="filterKey"
         v-model="selected"
         :headers="headers"
         :items="items"
@@ -28,7 +36,11 @@
       >
         <template slot="items" slot-scope="props">
           <td style="width:20px;">
-            <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
+            <v-checkbox
+              v-model="props.selected"
+              primary
+              hide-details
+            ></v-checkbox>
           </td>
           <!-- 站点 -->
           <td style="text-align: center;">
@@ -47,34 +59,51 @@
               target="_blank"
               :title="props.item.data.title"
               rel="noopener noreferrer nofollow"
-            >{{ props.item.data.title || props.item.data.link}}</a>
-            <span
-              v-else
-              :title="props.item.data.url"
-            >{{ props.item.data.title || props.item.data.url }}</span>
+              >{{ props.item.data.title || props.item.data.link }}</a
+            >
+            <span v-else :title="props.item.data.url">{{
+              props.item.data.title || props.item.data.url
+            }}</span>
             <br />
-            <span
-              class="sub-title"
-            >[ {{ getClientName(props.item.data.clientId||props.item.clientId) }} ] -> {{ props.item.data.savePath || $t("history.defaultPath") }}</span>
+            <span class="sub-title"
+              >[
+              {{
+                getClientName(props.item.data.clientId || props.item.clientId)
+              }}
+              ] ->
+              {{ props.item.data.savePath || $t("history.defaultPath") }}</span
+            >
           </td>
           <td>
             <v-icon
-              v-if="props.item.success===false"
+              v-if="props.item.success === false"
               color="error"
               :title="$t('history.fail')"
-            >close</v-icon>
-            <v-icon v-else color="success" :title="$t('history.success')">done</v-icon>
+              >close</v-icon
+            >
+            <v-icon v-else color="success" :title="$t('history.success')"
+              >done</v-icon
+            >
           </td>
           <td>{{ props.item.time | formatDate }}</td>
           <td>
             <!-- 重新下载 -->
-            <v-btn icon flat small @click="download(props.item)" :title="$t('history.download')">
+            <v-btn
+              icon
+              flat
+              small
+              @click="download(props.item)"
+              :title="$t('history.download')"
+            >
               <v-icon small>save_alt</v-icon>
             </v-btn>
 
             <!-- 下载到 -->
             <DownloadTo
-              :downloadOptions="{host: props.item.host, url: props.item.data.url}"
+              :downloadOptions="{
+                host: props.item.host,
+                url: props.item.data.url
+              }"
               flat
               icon
               small
@@ -101,28 +130,34 @@
     <!-- 删除确认 -->
     <v-dialog v-model="dialogRemoveConfirm" width="300">
       <v-card>
-        <v-card-title class="headline red lighten-2">{{ $t('history.removeConfirmTitle') }}</v-card-title>
+        <v-card-title class="headline red lighten-2">{{
+          $t("history.removeConfirmTitle")
+        }}</v-card-title>
 
-        <v-card-text>{{ $t('history.removeConfirm') }}</v-card-text>
+        <v-card-text>{{ $t("history.removeConfirm") }}</v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn flat color="info" @click="dialogRemoveConfirm=false">
+          <v-btn flat color="info" @click="dialogRemoveConfirm = false">
             <v-icon>cancel</v-icon>
-            <span class="ml-1">{{ $t('history.cancel') }}</span>
+            <span class="ml-1">{{ $t("history.cancel") }}</span>
           </v-btn>
           <v-btn color="error" flat @click="remove">
             <v-icon>check_circle_outline</v-icon>
-            <span class="ml-1">{{ $t('history.ok') }}</span>
+            <span class="ml-1">{{ $t("history.ok") }}</span>
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-    <v-snackbar v-model="haveError" top :timeout="3000" color="error">{{ errorMsg }}</v-snackbar>
-    <v-snackbar v-model="haveSuccess" bottom :timeout="3000" color="success">{{ successMsg }}</v-snackbar>
+    <v-snackbar v-model="haveError" top :timeout="3000" color="error">{{
+      errorMsg
+    }}</v-snackbar>
+    <v-snackbar v-model="haveSuccess" bottom :timeout="3000" color="success">{{
+      successMsg
+    }}</v-snackbar>
   </div>
 </template>
 <script lang="ts">
@@ -152,7 +187,8 @@ export default Vue.extend({
       haveError: false,
       haveSuccess: false,
       successMsg: "",
-      siteCache: {} as Dictionary<any>
+      siteCache: {} as Dictionary<any>,
+      filterKey: ""
     };
   },
 
