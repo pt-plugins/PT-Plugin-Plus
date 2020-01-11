9d579d09ee2ded546dab924c258e4012ce703746
Priston <priston910423@outlook.com>
2020-01-12 01:43:03 +0800

    BHD 脚本适应获取做种信息的修改

--- resource/sites/beyond-hd.me/getSearchResult.js
+++ resource/sites/beyond-hd.me/getSearchResult.js
@@ -10,6 +10,7 @@
       options.isLogged = true;
 
       this.haveData = true;
+      this.site = options.site;
     }
 
     /**
@@ -25,7 +26,6 @@
       let table = options.page.find(selector);
       // 获取种子列表行
       let rows = table.find("> tbody > tr");
-      console.log("rows.length", rows.length);
       if (rows.length == 0) {
         options.status = ESearchResultParseStatus.torrentTableIsEmpty;
         return [];
@@ -38,7 +38,6 @@
         beginRowIndex = 1;
         header = rows.eq(0).find("th,td");
       }
-      console.log("header.length", header.length);
 
       // 用于定位每个字段所列的位置
       let fieldIndex = {
@@ -57,7 +56,9 @@
         // 发布人
         author: header.length - 1,
         // 分类
-        category: 1
+        category: 1,
+        progress: 11,
+        status: 11
       };
 
       if (site.url.lastIndexOf("/") != site.url.length - 1) {
@@ -143,16 +144,11 @@
 
       try {
         // 遍历数据行
-
         for (let index = beginRowIndex; index < rows.length; index++) {
           const row = rows.eq(index);
           let cells = row.find(">td");
 
           let title = row.find("a.torrent-name");
-
-          console.log("title", title);
-          console.log("title.length", title.length);
-
           if (title.length == 0) {
             continue;
           }
@@ -174,8 +170,8 @@
           }
 
           let data = {
-            title: title.text(),
-            subTitle: this.getSubTitle(title, row),
+            title: title.text().trim(),
+            subTitle: this.getSubTitle(title, row).trim(),
             link,
             url: url,
             size:
@@ -190,35 +186,37 @@
                     .eq(fieldIndex.time)
                     .find("span[title]")
                     .attr("title") ||
-                  cells.eq(fieldIndex.time).text() ||
+                  cells.eq(fieldIndex.time).text().trim() ||
                   "",
             author:
               fieldIndex.author == -1
                 ? ""
-                : cells.eq(fieldIndex.author).text() || "",
+                : cells.eq(fieldIndex.author).text().trim() || "",
             seeders:
               fieldIndex.seeders == -1
                 ? ""
-                : cells.eq(fieldIndex.seeders).text() || 0,
+                : cells.eq(fieldIndex.seeders).text().trim() || 0,
             leechers:
               fieldIndex.leechers == -1
                 ? ""
-                : cells.eq(fieldIndex.leechers).text() || 0,
+                : cells.eq(fieldIndex.leechers).text().trim() || 0,
             completed:
               fieldIndex.completed == -1
                 ? ""
-                : cells.eq(fieldIndex.completed).text() || 0,
+                : cells.eq(fieldIndex.completed).text().trim() || 0,
             comments:
               fieldIndex.comments == -1
                 ? ""
-                : cells.eq(fieldIndex.comments).text() || 0,
+                : cells.eq(fieldIndex.comments).text().trim() || 0,
             site: site,
             tags: Searcher.getRowTags(site, row),
-            entryName: options.entry.name,
+            entryName: options.entry.name.trim(),
             category:
               fieldIndex.category == -1
                 ? null
-                : this.getCategory(cells.eq(fieldIndex.category))
+                : this.getCategory(cells.eq(fieldIndex.category)),
+            progress: this.getFieldValue(row, cells, fieldIndex, "progress"),
+            status: this.getFieldValue(row, cells, fieldIndex, "status")
           };
           results.push(data);
         }
@@ -242,7 +240,31 @@
     getSubTitle(title, row) {
       return "";
     }
+    
+    getFieldValue(row, cells, fieldIndex, fieldName, returnCell) {
+      let parent = row;
+      let cell = null;
+      if (
+        cells &&
+        fieldIndex &&
+        fieldIndex[fieldName] !== undefined &&
+        fieldIndex[fieldName] !== -1
+      ) {
+        cell = cells.eq(fieldIndex[fieldName]);
+        parent = cell || row;
+      }
 
+      let result = Searcher.getFieldValue(this.site, parent, fieldName);
+
+      if (!result && cell) {
+        if (returnCell) {
+          return cell;
+        }
+        result = cell.text().trim();
+      }
+      if(result == "")return null;
+      return result;
+    }
     /**
      * 获取分类
      * @param {*} cell 当前列
