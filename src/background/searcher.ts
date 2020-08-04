ed7486b2b5b70982906e3ca05e95803b6e33f13c
Rhilip <rhilipruan@gmail.com>
2020-08-04 21:17:52 +0800

    pref: TTG原cfDecodeEmail使用公用方法替换

--- src/background/searcher.ts
+++ src/background/searcher.ts
@@ -937,10 +937,10 @@ export class Searcher {
    * @param {*} encodedString
    */
   public cfDecodeEmail(encodedString: string) {
-    var email = "",
-      r = parseInt(encodedString.substr(0, 2), 16),
-      n,
-      i;
+    let email = "",
+        r = parseInt(encodedString.substr(0, 2), 16),
+        n,
+        i;
     for (n = 2; encodedString.length - n; n += 2) {
       i = parseInt(encodedString.substr(n, 2), 16) ^ r;
       email += String.fromCharCode(i);
