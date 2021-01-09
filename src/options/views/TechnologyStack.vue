f14bbfbddca07da65632c8ee959614f378ebd67c
Rhilip <rhilipruan@gmail.com>
2021-01-09 13:34:51 +0800

    dev(Depends): 参考项目中NPM库部分从package.json自动获取
    refs: https://github.com/ronggang/PT-Plugin-Plus/commit/9ba08c64a2ca06e9eebbf148d590c993a01047c2#commitcomment-45751445

--- src/options/views/TechnologyStack.vue
+++ src/options/views/TechnologyStack.vue
@@ -3,21 +3,21 @@
     <v-alert :value="true" type="info">{{ $t('reference.title') }}</v-alert>
     <v-card>
       <v-data-table
-        :headers="headers"
-        :items="items"
-        :pagination.sync="pagination"
-        item-key="name"
-        class="elevation-1"
-        hide-actions
+          :headers="headers"
+          :items="items"
+          :pagination.sync="pagination"
+          class="elevation-1"
+          hide-actions
+          item-key="name"
       >
         <template slot="items" slot-scope="props">
           <td>{{ props.item.name }}</td>
           <td>{{ props.item.ver }}</td>
           <td>
             <a
-              :href="props.item.url"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
+                :href="props.item.url"
+                rel="noopener noreferrer nofollow"
+                target="_blank"
             >{{ props.item.url }}</a>
           </td>
         </template>
@@ -28,6 +28,18 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
+import axios from 'axios';
+
+const rawDependencies = require('@/../package.json').dependencies;
+const dependencies = Object.entries(rawDependencies).map(value => {
+  const [name, version] = value
+  return {
+    name: name,
+    ver: (version as string),
+    url: `https://www.npmjs.com/package/${name}`
+  }
+})
+
 export default Vue.extend({
   data() {
     return {
@@ -35,61 +47,8 @@ export default Vue.extend({
         rowsPerPage: -1
       },
       items: [
-        {
-          name: "Vue",
-          ver: "2.5.17",
-          url: "https://github.com/vuejs/vue"
-        },
-        {
-          name: "vue-router",
-          ver: "3.0.1",
-          url: "https://github.com/vuejs/vue-router"
-        },
-        {
-          name: "vuex",
-          ver: "3.0.1",
-          url: "https://github.com/vuejs/vuex"
-        },
-        {
-          name: "Vuetify",
-          ver: "1.3.0",
-          url: "https://github.com/vuetifyjs/vuetify"
-        },
-        {
-          name: "Notice.js",
-          ver: "0.4.0",
-          url: "https://github.com/alihesari/notice.js"
-        },
-        {
-          name: "FileSaver.js",
-          ver: "2.0.0",
-          url: "https://github.com/eligrey/FileSaver.js"
-        },
-        {
-          name: "JavaScript MD5",
-          ver: "2.10.0",
-          url: "https://github.com/blueimp/JavaScript-MD5"
-        },
-        {
-          name: "jQuery",
-          ver: "1.9.1",
-          url: "https://github.com/jquery/jquery"
-        },
-        {
-          name: "webpack",
-          ver: "4.26.1",
-          url: "https://github.com/webpack/webpack"
-        },
-        {
-          name: "TypeScript",
-          ver: "3.2.2",
-          url: "https://github.com/Microsoft/TypeScript"
-        },
-        {
-          name: "Material design icons",
-          ver: "3.0.0",
-          url: "https://github.com/google/material-design-icons"
-        },
+        ...dependencies,
+        // 其他一些不属于NPM依赖的参考项目
         {
           name: "PT-Plugin Rhilip修改版",
           ver: "0.0.9",
@@ -99,31 +58,44 @@ export default Vue.extend({
           name: "Jackett",
           ver: "latest",
           url: "https://github.com/Jackett/Jackett"
-        },
-        {
-          name: "dom-to-image",
-          ver: "latest",
-          url: "https://github.com/tsayen/dom-to-image"
-        },
-        {
-          name: "Highcharts",
-          ver: "7.1.1",
-          url: "https://www.highcharts.com"
         }
       ]
     };
   },
 
+  created() {
+    const cacheDependMetaData = JSON.parse(localStorage.getItem('depend-metadata') || '{}')
+
+    // 延迟替换依赖的url项
+    setTimeout(async () => {
+      for (let i = 0; i < this.items.length; i++) {
+        let {name, url} = this.items[i]
+        if (url.match(/npmjs/)) {
+          if (cacheDependMetaData[name]) {
+            url = cacheDependMetaData[name]
+          } else {
+            try {
+              const req = await axios.get(`https://registry.npm.taobao.org/${name}`)
+              if (req.data?.homepage) {
+                url = cacheDependMetaData[name] = req.data?.homepage
+              }
+            } catch (e) {
+              //
+            }
+          }
+          this.items[i].url = url
+        }
+      }
+      localStorage.setItem('depend-metadata', JSON.stringify(cacheDependMetaData))
+    }, 1e3);
+  },
+
   computed: {
     headers(): Array<any> {
       return [
-        {
-          text: this.$t("reference.headers.name"),
-          align: "left",
-          value: "name"
-        },
-        { text: this.$t("reference.headers.ver"), align: "left", value: "ver" },
-        { text: this.$t("reference.headers.url"), align: "left", value: "url" }
+        {text: this.$t("reference.headers.name"), align: "left", value: "name"},
+        {text: this.$t("reference.headers.ver"), align: "left", value: "ver"},
+        {text: this.$t("reference.headers.url"), align: "left", value: "url"}
       ];
     }
   }
