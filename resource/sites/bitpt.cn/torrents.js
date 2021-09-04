eb34357dde48a94375b0cc8b6c57036767aaf53c
wanicca <wanicca@163.com>
2021-09-04 21:00:32 +0800

    增加bitpt（含用户信息、搜索、种子页面插件）、zhixing（含用户信息、搜索，不含插件）

---
+++ resource/sites/bitpt.cn/torrents.js
@@ -0,0 +1,60 @@
+(function($) {
+  console.log("this is torrent.js");
+  class App extends window.NexusPHPCommon {
+    init() {
+      this.initButtons();
+      this.initFreeSpaceButton();
+      // 设置当前页面
+      PTService.pageApp = this;
+    }
+
+    /**
+     * 初始化按钮列表
+     */
+    initButtons() {
+      this.initListButtons();
+    }
+
+    /**
+     * 获取下载链接
+     */
+    getDownloadURLs() {
+      let links = $("a[href*='download.php'][href*='ssl=yes']").toArray();
+      let siteURL = PTService.site.url;
+      if (siteURL.substr(-1) != "/") {
+        siteURL += "/";
+      }
+
+      if (links.length == 0) {
+        return this.t("getDownloadURLsFailed"); //"获取下载链接失败，未能正确定位到链接";
+      }
+
+      let urls = $.map(links, item => {
+        let url =
+          $(item).attr("href") ;
+        if (url) {
+          if (url.substr(0, 1) === "/") {
+            url = url.substr(1);
+          }
+          url = siteURL + url;
+
+        }
+        return url;
+      });
+
+      return urls;
+    }
+
+    /**
+     * 确认大小是否超限
+     */
+    confirmWhenExceedSize() {
+      return this.confirmSize(
+        $(".torrents").find(
+          "td:contains('MB'),td:contains('GB'),td:contains('TB')"
+        )
+      );
+    }
+  }
+  new App().init();
+})(jQuery);
