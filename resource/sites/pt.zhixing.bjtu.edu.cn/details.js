16d57fd08d9d94a86730186bb9de6b0aab93c0f4
wanicca <wanicca@163.com>
2021-09-04 21:32:57 +0800

    增加知行的种子详情页面

---
+++ resource/sites/pt.zhixing.bjtu.edu.cn/details.js
@@ -0,0 +1,56 @@
+(function ($, window) {
+  console.log("this is details.js");
+  class App extends window.NexusPHPCommon {
+    init() {
+      this.initButtons();
+      // 设置当前页面
+      PTService.pageApp = this;
+    }
+    /**
+     * 初始化按钮列表
+     */
+    initButtons() {
+      if (this.getDownloadURL()) {
+        this.initDetailButtons();
+      }
+    }
+
+    /**
+     * 获取下载链接
+     */
+    getDownloadURL() {
+      let query = $("a[href*='/download/']");
+      let url = "";
+      if (query.length > 0) {
+        url = query.attr("href");
+      }
+      if (!url) {
+        return "";
+      }
+
+      if (url.substr(0, 2) === '//') { 
+        url = `${location.protocol}${url}`;
+      } else if (url.substr(0, 1) === "/") {
+        url = `${location.origin}${url}`;
+      } else if (url.substr(0, 4) !== "http") {
+        url = `${location.origin}/${url}`;
+      }
+
+
+      return url;
+    }
+
+    /**
+     * 获取当前种子标题
+     */
+    getTitle() {
+      let title = $("div.torrent-title").text();
+      let datas = /\"(.*?)\"/.exec(title);
+      if (datas && datas.length > 1) {
+        return datas[1] || title;
+      }
+      return title;
+    }
+  };
+  (new App()).init();
+})(jQuery, window);
