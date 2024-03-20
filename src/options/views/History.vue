ec52afb62bbe883342ffff08f8796c31964dfcf9
栽培者 <ronggang.zhou@gmail.com>
2024-03-20 22:32:56 +0800

    feat: 下载历史增加批量重新下载及过滤功能； 该功能有助于按原路径重新下载丢失的种子。

--- src/options/views/History.vue
+++ src/options/views/History.vue
@@ -16,6 +16,66 @@
           <v-icon class="mr-2">clear</v-icon>
           {{ $t("history.clear") }}
         </v-btn>
+
+        <v-divider class="mx-3 mt-0" vertical></v-divider>
+
+        <v-btn
+          color="success"
+          @click="downloadSelected"
+          :disabled="selected.length == 0"
+        >
+          <v-icon class="mr-2">save_alt</v-icon>
+          {{ $t("history.download") }}
+        </v-btn>
+
+        <v-divider class="mx-3 mt-0" vertical></v-divider>
+
+        <v-autocomplete
+          v-model="selectedClient"
+          :items="clientItems"
+          :label="$t('history.filterServer')"
+          :menu-props="{ maxHeight: 'auto' }"
+          style="max-width: 500px"
+          :hint="selectedClient.address"
+          return-object
+          persistent-hint
+          item-text="name"
+          item-value="id"
+        >
+          <template slot="selection" slot-scope="{ item }">
+            <span>{{ item.name }}</span>
+          </template>
+          <template slot="item" slot-scope="data">
+            <v-list-tile-content>
+              <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+              <v-list-tile-sub-title
+                v-html="data.item.address"
+              ></v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action-text>{{
+              data.item.type
+            }}</v-list-tile-action-text>
+          </template>
+        </v-autocomplete>
+
+        <v-radio-group v-model="filterStatus" row class="ml-5">
+          <v-radio
+            :label="$t('history.success')"
+            color="success"
+            value="success"
+          ></v-radio>
+          <v-radio
+            :label="$t('history.fail')"
+            color="fail"
+            value="fail"
+          ></v-radio>
+          <v-radio
+            :label="$t('history.all')"
+            color="info"
+            value="all"
+          ></v-radio>
+        </v-radio-group>
+
         <v-spacer></v-spacer>
 
         <v-text-field
@@ -32,22 +92,23 @@
         :search="filterKey"
         v-model="selected"
         :headers="headers"
-        :items="items"
+        :items="filteredItems"
         :pagination.sync="pagination"
         item-key="data.url"
         select-all
         class="elevation-1"
       >
         <template slot="items" slot-scope="props">
-          <td style="width:20px;">
+          <td style="width: 20px">
             <v-checkbox
               v-model="props.selected"
               primary
               hide-details
             ></v-checkbox>
           </td>
+          <td>{{ props.index + 1 }}</td>
           <!-- 站点 -->
-          <td style="text-align: center;">
+          <td style="text-align: center">
             <div v-if="!!props.item.site">
               <v-avatar size="18">
                 <img :src="props.item.site.icon" />
@@ -106,7 +167,7 @@
             <DownloadTo
               :downloadOptions="{
                 host: props.item.host,
-                url: props.item.data.url
+                url: props.item.data.url,
               }"
               flat
               icon
@@ -192,8 +253,24 @@ export default Vue.extend({
       haveSuccess: false,
       successMsg: "",
       siteCache: {} as Dictionary<any>,
-      filterKey: ""
+      filterKey: "",
+      filteredItems: [] as any[],
+      filterStatus: "all",
+      selectedClient: {
+        address: ""
+      } as any,
+      clientItems: [] as any[]
     };
+  },
+
+  watch: {
+    selectedClient() {
+      this.resetItems();
+    },
+
+    filterStatus() {
+      this.resetItems();
+    }
   },
 
   methods: {
@@ -205,6 +282,41 @@ export default Vue.extend({
             this.getDownloadHistory();
           });
       }
+    },
+
+    resetItems() {
+      let clientId = "";
+
+      if (this.selectedClient && this.selectedClient.id) {
+        clientId = this.selectedClient.id;
+      }
+
+      if (clientId == "" && this.filterStatus == "all") {
+        this.getDownloadHistory();
+      } else {
+        this.filteredItems = [];
+        this.items.forEach((item: any) => {
+          let _clientId = item.clientId || item.data.clientId;
+
+          if (((this.filterStatus == "fail" && item.success === false) ||
+            (this.filterStatus == "success" && item.success !== false) ||
+            (this.filterStatus == "all")) && ((_clientId == clientId) || (clientId == ""))
+          ) {
+            let site = this.siteCache[item.host];
+            if (!site) {
+              site = this.options.sites.find((site: Site) => {
+                return site.host === item.host;
+              });
+              this.siteCache[item.host] = site;
+            }
+
+            item.site = site;
+            this.filteredItems.push(item);
+          }
+        });
+      }
+
+
     },
 
     removeSelected() {
@@ -242,6 +354,7 @@ export default Vue.extend({
       extension.sendRequest(EAction.getDownloadHistory).then((result: any) => {
         console.log("downloadHistory", result);
         this.items = [];
+        this.filteredItems = [];
         result.forEach((item: any) => {
           let site = this.siteCache[item.host];
           if (!site) {
@@ -252,7 +365,7 @@ export default Vue.extend({
           }
 
           item.site = site;
-
+          this.filteredItems.push(item);
           this.items.push(item);
         });
       });
@@ -267,7 +380,17 @@ export default Vue.extend({
       }
       return "";
     },
-    download(options: any) {
+
+    download(datas: any) {
+      let options = null;
+      let remainder = 0;
+      if (Array.isArray(datas)) {
+        options = datas.pop();
+        remainder = datas.length;
+      } else {
+        options = datas;
+      }
+
       console.log(options);
 
       this.haveSuccess = true;
@@ -290,7 +413,41 @@ export default Vue.extend({
             this.haveError = true;
             this.errorMsg = result.msg;
           }
+
+          if (remainder > 0) {
+            setTimeout(() => {
+              this.download(datas);
+            }, 500);
+          }
+        }).catch((result: any) => {
+          if (result.success) {
+            this.haveSuccess = true;
+            this.successMsg = result.msg;
+          } else {
+            this.haveError = true;
+            this.errorMsg = result.msg;
+          }
+
+          if (remainder > 0) {
+            setTimeout(() => {
+              this.download(datas);
+            }, 500);
+          }
         });
+    },
+
+    downloadSelected() {
+      if (this.selected && this.selected.length > 0) {
+        if (
+          confirm(
+            this.$t("history.downloadSelectedConfirm", {
+              count: this.selected.length
+            }).toString()
+          )
+        ) {
+          this.download(this.selected);
+        }
+      }
     },
 
     onError(msg: string) {
@@ -303,12 +460,25 @@ export default Vue.extend({
   },
 
   created() {
+    if (
+      this.$store.state.options.clients &&
+      this.$store.state.options.clients.length > 0
+    ) {
+      this.clientItems = this.$store.state.options.clients;
+    }
     this.getDownloadHistory();
   },
 
   computed: {
     headers(): Array<any> {
       return [
+        {
+          text: "№",
+          align: "left",
+          sortable: false,
+          value: "title",
+          width: 30
+        },
         {
           text: this.$t("history.headers.site"),
           align: "center",
