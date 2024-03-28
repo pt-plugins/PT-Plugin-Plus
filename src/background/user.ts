ccfda63bde2f520185fde81835d3374c4c04e6b8
TAINE <taine@live.com>
2024-03-28 18:51:24 +0800

    fix(user): do not set the cache value globally for user info ajax request
    fix(user): do not set the cache value globally for user info ajax request.
    do not set the cache value globally for user info ajax request, which fails the data getting of the seeding/seedingsize for some sites such as TorrentLeech.

--- src/background/user.ts
+++ src/background/user.ts
@@ -333,8 +333,7 @@ export class User {
         data: rule.requestContentType == "application/json" ? JSON.stringify(requestData) : requestData,
         contentType: rule.requestContentType == "application/json" ? "application/json" : "application/x-www-form-urlencoded",
         headers: rule.headers,
-        timeout: this.service.options.connectClientTimeout || 30000,
-        cache: false
+        timeout: this.service.options.connectClientTimeout || 30000
       })
         .done(result => {
           this.removeQueue(host, url);
