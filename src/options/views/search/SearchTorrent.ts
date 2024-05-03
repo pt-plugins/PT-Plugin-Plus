6887485207f993dd6143936ca3d884e3b6434b44
Trim21 <trim21.me@gmail.com>
2024-05-04 07:21:50 +0800

    fix

--- src/options/views/search/SearchTorrent.ts
+++ src/options/views/search/SearchTorrent.ts
@@ -587,11 +587,9 @@ export default Vue.extend({
           site.searchEntryConfig &&
           site.searchEntryConfig.skipNonLatinCharacters
         ) {
-          return false;
-        }
-
-        if (!this.key.match(/^[\p{Script_Extensions=Latin}\p{Script_Extensions=Common}]+$/gu)) {
-          return false;
+          if (!this.key.match(/^[\p{Script_Extensions=Latin}\p{Script_Extensions=Common}]+$/gu)) {
+            return false;
+          }
         }
         // 站点是否跳过IMDbId搜索
         if (
