41f7fb2b58276360fc0fb81305f7589ddba7532f
Rhilip <admin@rhilip.info>
2024-05-05 23:35:18 +0800

    clean: no need of `debug` folder

--- debug/src/buildResource.ts
+++
@@ -1,6 +0,0 @@
-import { BuildPlugin } from "./BuildPlugin";
-
-let buildPlugin = new BuildPlugin();
-buildPlugin.buildResource();
-buildPlugin.getSupportedSites();
-console.log("编译完成于：%s \n", new Date().toLocaleString());
