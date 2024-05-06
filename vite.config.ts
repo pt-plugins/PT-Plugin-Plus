0580f943a882f05ff6ba3bc0d2d827b815aebed4
Rhilip <admin@rhilip.info>
2024-05-06 20:17:49 +0800

    fix(manifest): miss min version update in new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import git from 'git-rev-sync'
 import buildResource from "./vite/buildResource";
 
 // https://vitejs.dev/config/
@@ -11,7 +13,17 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    buildResource()
+    buildResource(),
+    {
+      name: 'update_manifest_version',
+      buildEnd() {
+        const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json', 'utf-8'));
+        const build_number = git.count() % 65535;
+        manifest.version = `${manifest.version}.${build_number}`;
+        
+        fs.writeFileSync('./dist/manifest.json',  JSON.stringify(manifest, null, 2));
+      },
+    }
   ],
   resolve: {
     extensions: ['.ts', '.js', '.tsx', '.vue'],
