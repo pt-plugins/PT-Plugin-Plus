cfe51b171555dc71e1817eaf272a735b01af876d
Rhilip <admin@rhilip.info>
2024-05-06 20:30:02 +0800

    fix(manifest): miss public assets and can't find manifest file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import fs from 'node:fs'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import git from 'git-rev-sync'
@@ -7,6 +8,7 @@ import buildResource from "./vite/buildResource";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'public',
   build: {
     chunkSizeWarningLimit: 0,
     emptyOutDir: false
@@ -16,12 +18,13 @@ export default defineConfig({
     buildResource(),
     {
       name: 'update_manifest_version',
-      buildEnd() {
-        const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json', 'utf-8'));
+      closeBundle() {
+        const manifest_file_path = path.resolve(__dirname, './dist/manifest.json');
+        const manifest = JSON.parse(fs.readFileSync(manifest_file_path, 'utf-8'));
         const build_number = git.count() % 65535;
         manifest.version = `${manifest.version}.${build_number}`;
-        
-        fs.writeFileSync('./dist/manifest.json',  JSON.stringify(manifest, null, 2));
+
+        fs.writeFileSync(manifest_file_path, JSON.stringify(manifest, null, 2));
       },
     }
   ],
